42547ce15213094cf12c0e4aac1e3274e6442cb6
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2021-04-22 08:42:21 +1000

    add file

--- file_renamer.py
+++ file_renamer.py
@@ -1,5 +1,6 @@
 from zipfile import ZipFile
 import os
+import shutil
 
 def unzip(file, dest):
     try:
@@ -13,8 +14,20 @@ def rename(directory):
         new_name = entry[9:]
         os.rename(f'{directory}/{entry}',f'{directory}/{new_name}')
 
+def add_file(file, dest):
+    for entry in os.listdir(dest):
+        dash_index = entry.find("-")
+        new_name = entry[:dash_index]
+        print(new_name)
+        shutil.copyfile(file,f'{dest}/{new_name} - criteria.docx')
+
 # ----- MAIN PROGRAM -----
+# Global variables
 zip_file = "11.FIA1___Creating_with_Code___Investigation_Final.zip"
 destination = "student_files"
+criteria = "criteria.docx"
+
+# Function calls
 unzip(zip_file,destination)
 rename(destination)
+add_file(criteria, destination)
