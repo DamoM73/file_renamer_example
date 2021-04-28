7d21daa167f7c74914f389702e00630ac3591e89
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2021-04-28 12:59:20 +1000

    finished example

--- file_renamer.py
+++ file_renamer.py
@@ -1,6 +1,8 @@
 from zipfile import ZipFile
 import os
 import shutil
+import tkinter as tk
+from tkinter import filedialog
 
 def unzip(file, dest):
     try:
@@ -18,16 +20,56 @@ def add_file(file, dest):
     for entry in os.listdir(dest):
         dash_index = entry.find("-")
         new_name = entry[:dash_index]
-        print(new_name)
+        #print(new_name)
         shutil.copyfile(file,f'{dest}/{new_name} - criteria.docx')
 
-# ----- MAIN PROGRAM -----
-# Global variables
-zip_file = "11.FIA1___Creating_with_Code___Investigation_Final.zip"
-destination = "student_files"
-criteria = "criteria.docx"
+def zipped_btn():
+    root.zip_file = filedialog.askopenfile(initialdir=".").name
+    #print(root.zip_file)
 
-# Function calls
-unzip(zip_file,destination)
-rename(destination)
-add_file(criteria, destination)
+def folder_btn():
+    root.destination = filedialog.askdirectory(initialdir=".")
+    #print(root.destination)
+
+def criteria_btn():
+    root.criteria = filedialog.askopenfilename(initialdir=".")
+    print(root.criteria)
+
+def go_btn():
+    unzip(root.zip_file,root.destination)
+    rename(root.destination)
+    add_file(root.criteria, root.destination)
+
+# ----- MAIN PROGRAM -----
+# create window
+root = tk.Tk()
+root.geometry("600x400")
+root.title("File Renamer")
+
+# create elements
+tk.Label(root,text="File Renamer").grid(row=0, column=0, columnspan=2)
+
+# zipped folder picker
+tk.Label(root, text="Zipped file location").grid(row=1,column=0)
+tk.Button(root, text="Choose Folder", command=zipped_btn).grid(row=1, column=1)
+
+# desination folder picker
+tk.Label(root,text="Destination Folder").grid(row=2,column=0)
+tk.Button(root,text="Select Folder", command=folder_btn).grid(row=2,column=1)
+
+# criteria sheet picker
+tk.Label(root, text="Criteria Sheet").grid(row=3,column=0)
+tk.Button(root, text = "Choose Criteria Sheet", command=criteria_btn).grid(row=3,column=1)
+
+# go button
+tk.Button(root, text="Go",command=go_btn).grid(row=4,column=1)
+
+# Global variables
+root.zip_file = ""
+root.destination = ""
+root.criteria = ""
+
+
+
+# run mainloop
+root.mainloop()
